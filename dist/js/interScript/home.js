858491cc174dbeaaf600568eee70b0af18486f02
Peter Oluwasgun <petersheg@gmail.com>
2020-11-10 14:38:20 +0100

    included dist folder for deploy purpose

---
+++ dist/js/interScript/home.js
@@ -0,0 +1,28 @@
+AOS.init({
+    disable: 'phone'
+});
+$(function () {
+    $('.service_cat_list').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: false,
+        swipe: true,
+        arrows: true,
+        centerMode: true,
+        infinite: true,
+        appendArrows: $(".scroll_arrows"),
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
